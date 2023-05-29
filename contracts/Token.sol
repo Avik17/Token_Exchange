72ac850739316e6cb62a64a9729485999a891fbc
Avik17 <guptaavinash96@gmail.com>
2023-05-29 15:25:58 +0530

    3.Testing Contracts

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,7 +5,16 @@ import "hardhat/console.sol";
 
 contract Token{
 
-	string public name="My Token";
+	string public name;
+	string public symbol;
+	uint256 public decimals=18;
+	uint256 public totalSupply ;// 1000000 x 10^18
+
+	constructor(string memory _name,string memory _symbol,uint256 _totalSupply){
+		name=_name;
+		symbol=_symbol;
+		totalSupply=_totalSupply*(10**decimals);
+	}
 
 }
 
